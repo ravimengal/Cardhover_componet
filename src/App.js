9943be1cb29183b3941f148526676912bbf995d9
ravimengal <mengal22ravi@gmail.com>
2021-09-14 07:34:47 -0700

    first commit card hover effect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Cardhover from './components/Cardhover/Cardhover';
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>hello card hover effect</h1>
+    <Cardhover/>
     </div>
   );
 }
---
+++ src/components/Cardhover/Cardhover.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Cardhover.css'
+
+const Cardhover = () => {
+    return (
+        <div>
+            <div className="container">
+  <div className="row">
+    <div className="col-md-4">
+      <div className="card card-1">
+        <h3>Ionic Native</h3>
+        <p>A curated set of   ES5/ES6/TypeScript wrappers for plugins to easily add any native functionality to your Ionic apps.</p>
+      </div>
+    </div>
+   
+    </div>
+  </div>
+</div>
+     
+    )
+}
+
+export default Cardhover
