ed9b2544222cbc7391fde7c02b515a07f3c8ca24
ravimengal <mengal22ravi@gmail.com>
2021-09-14 07:42:01 -0700

    adding more card

--- src/components/Cardhover/Cardhover.js
+++ src/components/Cardhover/Cardhover.js
@@ -7,7 +7,19 @@ const Cardhover = () => {
             <div className="container">
   <div className="row">
     <div className="col-md-4">
-      <div className="card card-1">
+      <div className="card">
+        <h3>Ionic Native</h3>
+        <p>A curated set of   ES5/ES6/TypeScript wrappers for plugins to easily add any native functionality to your Ionic apps.</p>
+      </div>
+      <div className="card">
+        <h3>Ionic Native</h3>
+        <p>A curated set of   ES5/ES6/TypeScript wrappers for plugins to easily add any native functionality to your Ionic apps.</p>
+      </div>
+      <div className="card">
+        <h3>Ionic Native</h3>
+        <p>A curated set of   ES5/ES6/TypeScript wrappers for plugins to easily add any native functionality to your Ionic apps.</p>
+      </div>
+      <div className="card">
         <h3>Ionic Native</h3>
         <p>A curated set of   ES5/ES6/TypeScript wrappers for plugins to easily add any native functionality to your Ionic apps.</p>
       </div>
